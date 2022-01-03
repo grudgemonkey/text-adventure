634b1e73a3e95915acf89ab037527d6d9da38c13
grudgemonkey <grudgemonkey65@gmail.com>
2022-01-03 13:37:03 -0500

    added Scripts directory and gameplay campaigns

---
+++ Scripts/citizen-path.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+echo ERROR: citizen path is not set up yet
+../text-adventure.sh
---
+++ Scripts/soldier-path.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+echo "welcome to the training room"
+echo "your training will start soon"
+echo "but first, can we have your name?"
+read;
+Name=${REPLY}
+sleep 3
+echo "thank you, $Name. Please head to the training room, where you will learn basic combat"
+
