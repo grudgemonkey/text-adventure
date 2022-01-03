a6881c60be15b08275a4a7c182c204f36118dead
grudgemonkey <grudgemonkey65@gmail.com>
2022-01-03 13:12:46 -0500

    modified main gameplay script

--- text-adventure.sh
+++ text-adventure.sh
@@ -1,2 +1,21 @@
 #!/usr/bin/bash
-echo text will go here
+
+#this is prototype gameplay
+
+#echo text will go here
+
+echo "welcome to the land of xed, where all hell has broken loose..."
+sleep 5
+echo "pick a role..."
+sleep 1
+echo [1] a soldier
+echo [2] a citizen
+read;
+if [[ ${REPLY} = 1 ]]
+then ./Scripts/soldier-path.sh
+elif [[ ${REPLY} = 2 ]]
+then ./Scripts/citizen-path.sh
+elif [[ ${REPLY} = "exit" ]]
+then 
+   echo "exiting..."
+fi
